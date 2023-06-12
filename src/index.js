75e21a819971bb83b163243f1978148eb44a5fb1
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2023-06-12 13:20:47 -0700

    testing removing serviceworker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; 
 import './index.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 /* GLOBAL VARIABLES */
@@ -14,4 +14,4 @@ window.$secondaryLanguageIconId = 'secondary-lang-icon';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-serviceWorker.register();
+// serviceWorker.register();
